2e9af95376be79e4f1e98423e103b43e3c799610
Josh <eljosho1998@gmail.com>
2018-05-21 12:23:50 +1000

    Movement added, added Collision function to be worked on later.

--- Funk Vengeance/Assets/Scripts/Josh/PlayerMovement.cs
+++ Funk Vengeance/Assets/Scripts/Josh/PlayerMovement.cs
@@ -4,13 +4,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    [Tooltip("Player Movement speed.")]
+    public float movementSpeed;
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    void Update()
+    {
+        // Move upwards
+        if (Input.GetKey("w"))
+        {
+            gameObject.transform.position += new Vector3(0.0f, 0.0f, movementSpeed);
+        }
+        // Move downwards
+        if (Input.GetKey("s"))
+        {
+            gameObject.transform.position += new Vector3(0.0f, 0.0f, -movementSpeed);
+        }
+        // Move leftwards
+        if (Input.GetKey("a"))
+        {
+            gameObject.transform.position += new Vector3(-movementSpeed, 0.0f, 0.0f);
+        }
+        // Move rightwards
+        if (Input.GetKey("d"))
+        {
+            gameObject.transform.position += new Vector3(movementSpeed, 0.0f, 0.0f);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // If colliding with an Enemy
+        if (collision.collider.tag == "Enemy")
+        {
+
+        }
+        // If colliding with the Target
+        if (collision.collider.tag == "Target")
+        {
+
+        }
+        // If colliding with the Door
+        if (collision.collider.tag == "Door")
+        {
+
+        }
+        // If colliding with a Powerup
+        if (collision.collider.tag == "Powerup")
+        {
+
+        }
+    }
 }
