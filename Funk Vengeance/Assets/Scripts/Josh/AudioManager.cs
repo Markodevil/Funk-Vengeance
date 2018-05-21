8a9edc053a234c2b3a5353f157f0a044ecebbced
Josh <eljosho1998@gmail.com>
2018-05-21 14:27:09 +1000

    Added AudioManager.

---
+++ Funk Vengeance/Assets/Scripts/Josh/AudioManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    // List of Sound Clips
+    public static AudioClip CaughtClip, Stab1Clip, Stab2Clip, TargetDownClip, VictoryClip, FailureClip;
+    // Audio Source
+    static AudioSource am;
+
+    // Sound Clips
+    // Variables that appear in the Unity Editor, for quick and easy replacement
+    [SerializeField]
+    [Tooltip("Audio clip for getting caught.")]
+    public AudioClip Caught_SFX;
+    [SerializeField]
+    [Tooltip("Audio clip for stabbing.")]
+    public AudioClip Stab1_SFX;
+    [SerializeField]
+    [Tooltip("Audio clip for stabbing.")]
+    public AudioClip Stab2_SFX;
+    [SerializeField]
+    [Tooltip("Audio clip for the target doing down.")]
+    public AudioClip TargetDown_SFX;
+    [SerializeField]
+    [Tooltip("Audio clip when the victory is achieved.")]
+    public AudioClip Victory_SFX;
+    [SerializeField]
+    [Tooltip("Audio clip when you have failed.")]
+    public AudioClip Failure_SFX;
+
+    // Initialization
+    void Start ()
+    {
+        CaughtClip = Caught_SFX;
+        Stab1Clip = Stab1_SFX;
+        Stab2Clip = Stab2_SFX;
+        TargetDownClip = TargetDown_SFX;
+        VictoryClip = Victory_SFX;
+        FailureClip = Failure_SFX;
+    }
+	
+    // Update
+	void Update ()
+    {
+		
+	}
+
+    // Play Sound
+    // Switch Statement to play a differing sound for each case
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            // Caught sound
+            case "Caught":
+                am.PlayOneShot(CaughtClip, 1f);
+                break;
+            // Stab sound
+            case "Stab1":
+                am.PlayOneShot(Stab1Clip, 1f);
+                break;
+            // Stab sound
+            case "Stab2":
+                am.PlayOneShot(Stab2Clip, 1f);
+                break;
+            // Target Down sound
+            case "TargetDown":
+                am.PlayOneShot(TargetDownClip, 1f);
+                break;
+            // Player Failure sound
+            case "Victory":
+                am.PlayOneShot(VictoryClip, 1f);
+                break;
+            // Player Victory sound
+            case "Failure":
+                am.PlayOneShot(FailureClip, 1f);
+                break;
+        }
+    }
+}
