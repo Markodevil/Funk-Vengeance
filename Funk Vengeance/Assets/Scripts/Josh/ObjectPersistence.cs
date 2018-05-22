f706cc6ec2779084deb3280e5dd8cbdc90cc4873
Josh <eljosho1998@gmail.com>
2018-05-22 15:19:14 +1000

    Updated scripts, created ObjectPersistence script, created scenes.

---
+++ Funk Vengeance/Assets/Scripts/Josh/ObjectPersistence.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPersistence : MonoBehaviour {
+
+    // On Awake
+	void Awake ()
+    {
+        // Prevent the object from being deleted upon a scene being loaded
+        DontDestroyOnLoad(gameObject);
+   
+        // Store a duplicate of the object into a variable if it exists
+        GameObject duplicate = GameObject.Find(gameObject.name);
+
+        // If said variable exists, then delete it
+        if (duplicate != gameObject)
+        {
+            DestroyImmediate(duplicate);
+            Debug.Log("Destroying Duplicate.");
+        }
+        else
+        {
+            Debug.Log("No Duplicates exist.");
+        }
+    }	
+}
--- Funk Vengeance/Assets/Scripts/Josh/UIManager.cs
+++ Funk Vengeance/Assets/Scripts/Josh/UIManager.cs
@@ -18,24 +18,53 @@ public class UIManager : MonoBehaviour {
     [Tooltip("Canvas for the Failure Menu.")]
     public Canvas FailureCanvas;
 
+    bool isPaused = false;
+
     // Start
     void Start()
     {
+        // Get the canvas component, then set it to active
         MainMenuCanvas.GetComponent<Canvas>();
+        MainMenuCanvas.gameObject.SetActive(true);
+        // Get the canvas component, then set it to inactive
         PauseCanvas.GetComponent<Canvas>();
+        PauseCanvas.gameObject.SetActive(false);
+        // Get the canvas component, then set it to inactive
         VictoryCanvas.GetComponent<Canvas>();
+        VictoryCanvas.gameObject.SetActive(false);
+        // Get the canvas component, then set it to inactive
         FailureCanvas.GetComponent<Canvas>();
+        FailureCanvas.gameObject.SetActive(false);
+
+        isPaused = false;
     }
 
     // Update
     void Update()
     {
-        if (Input.GetKey(KeyCode.KeypadEnter))
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) && (isPaused == false))
         {
             // Enable Pause canvas
             PauseCanvas.gameObject.SetActive(true);
             // Set timescale to 0 to pause the scene
+            Debug.Log(isPaused);
+            isPaused = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.KeypadEnter) && (isPaused == true))
+        {
+            // Enable Pause canvas
+            PauseCanvas.gameObject.SetActive(false);
+            // Set timescale to 0 to pause the scene
+            isPaused = false;
+        }
+
+        if (isPaused == true)
+        {
             Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
         }
     }
 
@@ -45,7 +74,7 @@ public class UIManager : MonoBehaviour {
         // Disable Main Menu canvas
         MainMenuCanvas.gameObject.SetActive(false);
         // Load up the Level01 scene
-        SceneManager.LoadScene("Test_01", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Test_01", LoadSceneMode.Single);
     }
     public void MainMenu_Quit()
     {
@@ -58,51 +87,51 @@ public class UIManager : MonoBehaviour {
     {
         // Disable Pause canvas
         PauseCanvas.gameObject.SetActive(false);
-        // Set timescale to 1 to unpause the scene
-        Time.timeScale = 1;
+        // Set isPaused to true to pause the scene
+        isPaused = true;
     }
     public void Gameplay_Pause_Quit()
     {
         // Disable Pause canvas
         PauseCanvas.gameObject.SetActive(false);
-        // Set timescale to 1 to unpause the scene
-        Time.timeScale = 1;
+        // Set isPaused to false to unpause the scene
+        isPaused = false;
         // Return to the Main Menu scene
-        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Josh_MainMenu", LoadSceneMode.Single);
     }
 
     public void Gameplay_Victory_LeaveArea_Yes()
     {
         // Disable Victory canvas
         VictoryCanvas.gameObject.SetActive(false);
-        // Set timescale to 1 to unpause the scene
-        Time.timeScale = 1;
+        // Set isPaused to false to unpause the scene
+        isPaused = false;
         // Return to the Main Menu scene
-        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Josh_MainMenu", LoadSceneMode.Single);
     }
     public void Gameplay_Victory_LeaveArea_No()
     {
         // Disable Victory canvas
         VictoryCanvas.gameObject.SetActive(false);
-        // Set timescale to 1 to unpause the scene
-        Time.timeScale = 1;
+        // Set isPaused to false to unpause the scene
+        isPaused = false;
     }
     public void Gameplay_Victory_Quit()
     {
         // Disable Victory canvas
         VictoryCanvas.gameObject.SetActive(false);
-        // Set timescale to 1 to unpause the scene
-        Time.timeScale = 1;
+        // Set isPaused to false to unpause the scene
+        isPaused = false;
         // Return to the Main Menu scene
-        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Josh_MainMenu", LoadSceneMode.Single);
     }
 
     public void Gameplay_Failure_Retry()
     {
         // Disable Failure canvas
         FailureCanvas.gameObject.SetActive(false);
-        // Set timescale to 1 to unpause the scene
-        Time.timeScale = 1;
+        // Set isPaused to false to unpause the scene
+        isPaused = false;
         // Reset the current level
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
@@ -110,9 +139,9 @@ public class UIManager : MonoBehaviour {
     {
         // Disable Failure canvas
         FailureCanvas.gameObject.SetActive(false);
-        // Set timescale to 1 to unpause the scene
-        Time.timeScale = 1;
+        // Set isPaused to false to unpause the scene
+        isPaused = false;
         // Return to the Main Menu scene
-        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Josh_MainMenu", LoadSceneMode.Single);
     }
 }
