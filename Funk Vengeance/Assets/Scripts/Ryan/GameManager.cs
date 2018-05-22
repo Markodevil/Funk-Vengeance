3360d78c2ff5d663e613864525c304eb2b9498e0
Fryzeec <32441156+Fryzeec@users.noreply.github.com>
2018-05-22 12:28:34 +1000

    Finished game Loop just got to set up and game over screen and menu screen

---
+++ Funk Vengeance/Assets/Scripts/Ryan/GameManager.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject blade;
+    private bool KillMode = false;
+    private bool hasBlade = false;
+
+    public GameObject player;
+    private bool hasWon = false;
+
+    private int target;
+    public Canvas GameOverCanvas;
+
+    private int TargetPicker()
+    {
+        //random //1 - 6
+        switch(Random.Range(1,6))
+        {
+            case 1:
+                Debug.Log("The target is MR Blue");
+                return 1;
+            case 2:
+                Debug.Log("The target is MR Red");
+                return 2;
+            case 3:
+                Debug.Log("The target is MR Yellow");
+                return 3;
+            case 4:
+                Debug.Log("The target is MR Green");
+                return 4;
+            case 5:
+                Debug.Log("The target is MR Black");
+                return 5;
+            case 6:
+                Debug.Log("The target is MR White");
+                return 6;
+        }
+        return 6;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) &&  hasBlade == true)
+        {
+            KillMode = true;
+            Debug.Log("Ready to kill");
+        }
+        else if(Input.GetKeyUp(KeyCode.Space) && hasBlade == true)
+        {
+            KillMode = false;
+            Debug.Log("*Sheaths blade*");
+        }
+
+        //Player Re-try
+        if (Input.GetKeyDown(KeyCode.Y) && hasWon)
+        {
+            SceneManager.LoadScene("Main");
+        }
+        //Player Exit
+        if (Input.GetKeyDown(KeyCode.Y) && hasWon)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void RETRY()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Weapon")
+        {
+            Debug.Log("You have the blade");
+            //Kill mode is active 
+            hasBlade = true;
+            Destroy(other.gameObject);
+            target = TargetPicker();
+        }
+        if (other.tag == "Door")
+        {
+            Debug.Log("You win, do you whant to retry? (Hit Y/N)");
+
+            GameOverCanvas.gameObject.SetActive(true);
+            StopAllCoroutines();
+        }
+
+        if (KillMode == true)
+        {
+            switch (other.name)
+            {
+                case "MR BlUE":
+                    if (target == 1)
+                    {
+                        Debug.Log("You Got Him Now get to the Door");
+                        hasWon = true;
+                    }
+                    else
+                    {
+                        Debug.Log("You killed the wrong guy!!!");
+                        StopAllCoroutines();
+                        Debug.Log("GameOver");
+                        SceneManager.LoadScene("Game Over");
+                    }
+                    break;
+                case "MR RED":
+                    if (target == 2)
+                        Debug.Log("You Got Him Now get to the Door");
+                    else
+                    {
+                        Debug.Log("You killed the wrong guy!!!");
+                        StopAllCoroutines();
+                        Debug.Log("GameOver");
+                        SceneManager.LoadScene("Game Over");
+                    }
+                    break;
+                case "MR YELLOW":
+                    if (target == 3)
+                        Debug.Log("You Got Him Now get to the Door");
+                    else
+                    {
+                        Debug.Log("You killed the wrong guy!!!");
+                        StopAllCoroutines();
+                        Debug.Log("GameOver");
+                        SceneManager.LoadScene("Game Over");
+                    }
+                    break;
+                case "MR GREEN":
+                    if (target == 4)
+                        Debug.Log("You Got Him Now get to the Door");
+                    else
+                    {
+                        Debug.Log("You killed the wrong guy!!!");
+                        StopAllCoroutines();
+                        Debug.Log("GameOver");
+                        SceneManager.LoadScene("Game Over");
+                    }
+                    break;
+                case "MR BLACK":
+                    if (target == 5)
+                        Debug.Log("You Got Him Now get to the Door");
+                    else
+                    {
+                        Debug.Log("You killed the wrong guy!!!");
+                        StopAllCoroutines();
+                        Debug.Log("GameOver");
+                        SceneManager.LoadScene("Game Over");
+                    }
+                    break;
+                case "MR White":
+                    if (target == 6)
+                        Debug.Log("You Got Him Now get to the Door");
+                    else
+                    {
+                        Debug.Log("You killed the wrong guy!!!");
+                        StopAllCoroutines();
+                        Debug.Log("GameOver");
+                        SceneManager.LoadScene("Game Over");
+                    }
+                    break;
+            }
+        }
+    }
+}
